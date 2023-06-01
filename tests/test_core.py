7fc07512882f9217305fe88f285e6c05dfbbb8ab
pnadolny13 <patnadolny@gmail.com>
2023-06-01 16:46:35 -0400

    refresh repo

---
+++ tests/test_core.py
@@ -0,0 +1,35 @@
+"""Tests standard target features using the built-in SDK tests library."""
+
+from __future__ import annotations
+
+import typing as t
+
+import pytest
+from singer_sdk.testing import get_target_test_class
+
+from target_snowflake.target import TargetSnowflake
+
+SAMPLE_CONFIG: dict[str, t.Any] = {}
+
+
+# Run standard built-in target tests from the SDK:
+StandardTargetTests = get_target_test_class(
+    target_class=TargetSnowflake,
+    config=SAMPLE_CONFIG,
+)
+
+
+class TestTargetSnowflake(StandardTargetTests):  # type: ignore[misc, valid-type]  # noqa: E501
+    """Standard Target Tests."""
+
+    @pytest.fixture(scope="class")
+    def resource(self):  # noqa: ANN201
+        """Generic external resource.
+
+        This fixture is useful for setup and teardown of external resources,
+        such output folders, tables, buckets etc. for use during testing.
+
+        Example usage can be found in the SDK samples test suite:
+        https://github.com/meltano/sdk/tree/main/tests/samples
+        """
+        return "resource"
