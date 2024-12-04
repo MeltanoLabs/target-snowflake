95e2296e559cd3c351e9922e1ba8af56622af6aa
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-12-03 21:30:28 -0600

    chore: Flush logger hanlders at exit (#298)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,23 @@
 """Test Configuration."""
 
 from __future__ import annotations
+
+import typing as t
+
+if t.TYPE_CHECKING:
+    import pytest
+
+
+def pytest_sessionfinish(session: pytest.Session, exitstatus: int) -> None:  # noqa: ARG001
+    """Session Finish."""
+    import logging
+
+    loggers: list[logging.Logger] = [
+        logging.getLogger(),
+        *list(logging.Logger.manager.loggerDict.values()),  # type: ignore[list-item]
+    ]
+
+    for logger in loggers:
+        handlers = getattr(logger, "handlers", [])
+        for handler in handlers:
+            logger.removeHandler(handler)
