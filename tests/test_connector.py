b2e9452fa7edbc3d3c6d3df4f661a632fc893a94
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-11-06 11:41:20 -0600

    packaging: Use PEP 621 and PEP 735

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import pytest
 import snowflake.sqlalchemy.custom_types as sct
-import sqlalchemy as sa
+from sqlalchemy import types
 
 from target_snowflake.connector import SnowflakeConnector, SnowflakeTimestampType
 from target_snowflake.snowflake_types import NUMBER, VARIANT
@@ -25,14 +25,14 @@ def connector():
         pytest.param({"type": ["number", "null"]}, sct.DOUBLE, id="number"),
         pytest.param({"type": ["string", "null"], "format": "date-time"}, sct.TIMESTAMP_NTZ, id="date-time"),
         # Upstream types
-        pytest.param({"type": ["string", "null"]}, sa.types.VARCHAR, id="string"),
-        pytest.param({"type": ["boolean", "null"]}, sa.types.BOOLEAN, id="boolean"),
-        pytest.param({"type": "string", "format": "time"}, sa.types.TIME, id="time"),
-        pytest.param({"type": "string", "format": "date"}, sa.types.DATE, id="date"),
-        pytest.param({"type": "string", "format": "uuid"}, sa.types.UUID, id="uuid"),
+        pytest.param({"type": ["string", "null"]}, types.VARCHAR, id="string"),
+        pytest.param({"type": ["boolean", "null"]}, types.BOOLEAN, id="boolean"),
+        pytest.param({"type": "string", "format": "time"}, types.TIME, id="time"),
+        pytest.param({"type": "string", "format": "date"}, types.DATE, id="date"),
+        pytest.param({"type": "string", "format": "uuid"}, types.UUID, id="uuid"),
     ],
 )
-def test_jsonschema_to_sql(connector: SnowflakeConnector, schema: dict, expected_type: type[sa.types.TypeEngine]):
+def test_jsonschema_to_sql(connector: SnowflakeConnector, schema: dict, expected_type: type[types.TypeEngine]):
     sql_type = connector.to_sql_type(schema)
     assert isinstance(sql_type, expected_type)
 
@@ -45,7 +45,7 @@ def test_jsonschema_to_sql(connector: SnowflakeConnector, schema: dict, expected
         ({"timestamp_type": SnowflakeTimestampType.TIMESTAMP_NTZ}, sct.TIMESTAMP_NTZ),
     ],
 )
-def test_datetime_to_sql(connector: SnowflakeConnector, config: dict, expected_type: type[sa.types.TypeEngine]):
+def test_datetime_to_sql(connector: SnowflakeConnector, config: dict, expected_type: type[types.TypeEngine]):
     connector.config.update(config)
     schema = {"type": ["string", "null"], "format": "date-time"}
     sql_type = connector.to_sql_type(schema)
@@ -54,41 +54,41 @@ def test_datetime_to_sql(connector: SnowflakeConnector, config: dict, expected_t
 
 def test_to_sql_type_with_max_varchar_length(connector: SnowflakeConnector):
     sql_type = connector.to_sql_type({"type": "string", "maxLength": 1_000_000})
-    assert isinstance(sql_type, sa.types.VARCHAR)
+    assert isinstance(sql_type, types.VARCHAR)
     assert sql_type.length == 1_000_000
 
     sql_type = connector.to_sql_type({"type": "string", "maxLength": SnowflakeConnector.max_varchar_length + 1})
-    assert isinstance(sql_type, sa.types.VARCHAR)
+    assert isinstance(sql_type, types.VARCHAR)
     assert sql_type.length == SnowflakeConnector.max_varchar_length
 
 
 def test_email_format(connector: SnowflakeConnector):
     sql_type = connector.to_sql_type({"type": "string", "format": "email"})
-    assert isinstance(sql_type, sa.types.VARCHAR)
+    assert isinstance(sql_type, types.VARCHAR)
     assert sql_type.length == 254
 
 
 def test_uri_format(connector: SnowflakeConnector):
     sql_type = connector.to_sql_type({"type": "string", "format": "uri"})
-    assert isinstance(sql_type, sa.types.VARCHAR)
+    assert isinstance(sql_type, types.VARCHAR)
     assert sql_type.length == 2083
 
 
 def test_hostname_format(connector: SnowflakeConnector):
     sql_type = connector.to_sql_type({"type": "string", "format": "hostname"})
-    assert isinstance(sql_type, sa.types.VARCHAR)
+    assert isinstance(sql_type, types.VARCHAR)
     assert sql_type.length == 253
 
 
 def test_ipv4_format(connector: SnowflakeConnector):
     sql_type = connector.to_sql_type({"type": "string", "format": "ipv4"})
-    assert isinstance(sql_type, sa.types.VARCHAR)
+    assert isinstance(sql_type, types.VARCHAR)
     assert sql_type.length == 15
 
 
 def test_ipv6_format(connector: SnowflakeConnector):
     sql_type = connector.to_sql_type({"type": "string", "format": "ipv6"})
-    assert isinstance(sql_type, sa.types.VARCHAR)
+    assert isinstance(sql_type, types.VARCHAR)
     assert sql_type.length == 45
 
 
@@ -101,6 +101,6 @@ def test_singer_decimal(connector: SnowflakeConnector):
             "scale": 18,
         },
     )
-    assert isinstance(sql_type, sa.types.DECIMAL)
+    assert isinstance(sql_type, types.DECIMAL)
     assert sql_type.precision == 38
     assert sql_type.scale == 18
